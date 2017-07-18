4b17abe323166c1dc73b4e8fcc4bb07d919f982c
Graham Ollis <plicease@cpan.org>
2017-07-18 01:46:13 -0400

    Initial commit

---
+++ lib/Test2/Tools/Compare/URL.pm
@@ -0,0 +1,10 @@
+package Test2::Tools::Compare::URL;
+
+use strict;
+use warnings;
+use 5.008001;
+
+# ABSTRACT: Compare a URL in your Test2 test
+# VERSION
+
+1;
---
+++ t/test2_tools_compare_url.t
@@ -0,0 +1,6 @@
+use Test2::V0;
+use Test2::Tools::Compare::URL;
+
+ok 1, 'todo';
+
+done_testing
